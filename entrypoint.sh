80b3c52f454b07897b15b806df68421d11b451ab
Sebastian Marines <sebastian0marines@gmail.com>
2021-07-26 11:46:13 -0500

    Create setup script

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,9 @@ done
 if ! psql postgresql://$PG_USER:$PG_PASSWD@$PG_HOSTNAME -lqt | cut -d \| -f 1 | grep -qw $PG_DATABASE; then
     echo "Database $PG_DATABASE does not exist"
     echo "Creating ..."
-    mix setup
+    mix ecto.setup
 fi
 
+mix deps.get
 mix ecto.migrate
 mix phx.server
